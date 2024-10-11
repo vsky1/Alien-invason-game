0bbc7b245716c36ebae1433ce1af98026cd7c819
Yawen Zhou <213232819@seu.edu.cn>
2024-10-11 19:58:44 +0800

    加入结束游戏快捷键与全屏模式

--- alien_invasion.py
+++ alien_invasion.py
@@ -12,7 +12,9 @@ class AlienInvasion:
         self.clock = pygame.time.Clock()
         self.settings=Settings()
 
-        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        self.screen = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
+        self.settings.screen_width=self.screen.get_rect().width
+        self.settings.screen_height=self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
 
         self.ship=Ship(self)
@@ -54,6 +56,8 @@ class AlienInvasion:
             self.ship.moving_right=True
         elif event.key==pygame.K_LEFT:
             self.ship.moving_left=True
+        elif event.key==pygame.K_q:
+            sys.exit()
 
     def _check_keyup_events(self,event):
         """Respond to key releases."""
