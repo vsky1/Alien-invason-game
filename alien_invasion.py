ff340326e386df9b77e641511d24ef45f98ffa54
Yawen Zhou <213232819@seu.edu.cn>
2024-10-11 19:51:22 +0800

    重构_check_events(),拆成两个方法

--- alien_invasion.py
+++ alien_invasion.py
@@ -43,16 +43,24 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = True
-            elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = False
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = False
+            elif event.type==pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type==pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self,event):
+        """Respond to keypresses."""
+        if event.key==pygame.K_RIGHT:
+            self.ship.moving_right=True
+        elif event.key==pygame.K_LEFT:
+            self.ship.moving_left=True
+
+    def _check_keyup_events(self,event):
+        """Respond to key releases."""
+        if event.key==pygame.K_RIGHT:
+            self.ship.moving_right=False
+        elif event.key==pygame.K_LEFT:
+            self.ship.moving_left=False
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
